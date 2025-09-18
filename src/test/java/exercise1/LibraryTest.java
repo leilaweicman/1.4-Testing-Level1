441a10f5543175b75dc655bd62e9f4a0273d6f82
leilaweicman <leilaweic@hotmail.com>
2025-09-18 13:19:38 +0200

    test(library): verify books list size after adding several books

--- src/test/java/exercise1/LibraryTest.java
+++ src/test/java/exercise1/LibraryTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class LibraryTest {
 
     @Test
-    void bookListIsNotNullOnNewLibrary() {
+    void booksListIsNotNullOnNewLibrary() {
         Library library = new Library();
         assertNotNull(library.getBooks());
     }
@@ -17,4 +17,13 @@ public class LibraryTest {
         assertEquals(0, library.getBooks().size());
     }
 
+    @Test
+    void booksListHasExpectedSizeAfterInsertBooks() {
+        Library library = new Library();
+        library.addBook(new Book("Book A"));
+        library.addBook(new Book("Book B"));
+        library.addBook(new Book("Book C"));
+
+        assertEquals( 3, library.getBooks().size(), "Library should contain 3 books after adding them");
+    }
 }
