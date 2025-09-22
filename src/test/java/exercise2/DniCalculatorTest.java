e11ce572607731ec6a3e12242af9b2e7f461735b
leilaweicman <leilaweic@hotmail.com>
2025-09-22 11:37:43 +0200

    test(dniCalculator): add parameterized tests for DniCalculator with 10 DNI samples

---
+++ src/test/java/exercise2/DniCalculatorTest.java
@@ -0,0 +1,34 @@
+package exercise2;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DniCalculatorTest {
+    private static Stream<Arguments> dniDataProvider() {
+        return Stream.of(
+                Arguments.of(12345678, 'Z'),
+                Arguments.of(87654321, 'X'),
+                Arguments.of(11111111, 'H'),
+                Arguments.of(22222222, 'J'),
+                Arguments.of(33333333, 'P'),
+                Arguments.of(44444444, 'A'),
+                Arguments.of(55555555, 'K'),
+                Arguments.of(66666666, 'Q'),
+                Arguments.of(77777777, 'B'),
+                Arguments.of(88888888, 'Y')
+        );
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("dniDataProvider")
+    void testDniLetterCalculation(int dniNumber, char expectedLetter) {
+        char result = DniCalculator.calculateLetter(dniNumber);
+        assertEquals(expectedLetter, result, () -> "Expected " + expectedLetter + " but got " + result + " for DNI " + dniNumber);
+    }
+}
