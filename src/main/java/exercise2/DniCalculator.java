8de4595b3fc837b39a87cfd32d35bbe01bbd0a96
leilaweicman <leilaweic@hotmail.com>
2025-09-22 11:53:53 +0200

    refactor: improve error handling for invalid DNI numbers

--- src/main/java/exercise2/DniCalculator.java
+++ src/main/java/exercise2/DniCalculator.java
@@ -5,8 +5,8 @@ public class DniCalculator {
     private static final String LETTERS = "TRWAGMYFPDXBNJZSQVHLCKE";
 
     public static char calculateLetter(int dniNumber) {
-        if (dniNumber < 0) {
-            throw new IllegalArgumentException("DNI number cannot be negative.");
+        if (dniNumber < 0|| dniNumber > 99_999_999) {
+            throw new IllegalArgumentException("DNI number must be an 8-digit positive number.");
         }
         int index = dniNumber % 23;
         return LETTERS.charAt(index);
