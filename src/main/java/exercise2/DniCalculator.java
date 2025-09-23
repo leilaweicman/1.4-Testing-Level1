fff03dfd42b042a72bec7709bb5ea9059f8000cf
leilaweicman <leilaweic@hotmail.com>
2025-09-23 11:54:03 +0200

    refactor(dni-calculator): rename LETTERS constant to DNI_LETTERS for clarity

--- src/main/java/exercise2/DniCalculator.java
+++ src/main/java/exercise2/DniCalculator.java
@@ -2,13 +2,13 @@ package exercise2;
 
 public class DniCalculator {
 
-    private static final String LETTERS = "TRWAGMYFPDXBNJZSQVHLCKE";
+    private static final String DNI_LETTERS = "TRWAGMYFPDXBNJZSQVHLCKE";
 
     public static char calculateLetter(int dniNumber) {
         if (dniNumber < 0|| dniNumber > 99_999_999) {
             throw new IllegalArgumentException("DNI number must be an 8-digit positive number.");
         }
         int index = dniNumber % 23;
-        return LETTERS.charAt(index);
+        return DNI_LETTERS.charAt(index);
     }
 }
