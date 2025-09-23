6a265cc5ea649bfb7f75424eb26e3d0afa0231ea
leilaweicman <leilaweic@hotmail.com>
2025-09-23 12:03:41 +0200

    refactor(dni-calculator): validate dni number must have 8 digits

--- src/main/java/exercise2/DniCalculator.java
+++ src/main/java/exercise2/DniCalculator.java
@@ -5,7 +5,7 @@ public class DniCalculator {
     private static final String DNI_LETTERS = "TRWAGMYFPDXBNJZSQVHLCKE";
 
     public static char calculateLetter(int dniNumber) {
-        if (dniNumber < 0|| dniNumber > 99_999_999) {
+        if (dniNumber < 10_000_000 || dniNumber > 99_999_999) {
             throw new IllegalArgumentException("DNI number must be an 8-digit positive number.");
         }
         int index = dniNumber % 23;
